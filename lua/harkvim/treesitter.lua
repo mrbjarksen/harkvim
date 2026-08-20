b0e5e338f83268f3ff6376c2e442823c593c27bd
Bjarki B. Harksen <bjarki@harksen.is>
2026-08-20 16:17:03 +0000

    fix(treesitter): refine starting autocommand

--- lua/harkvim/treesitter.lua
+++ lua/harkvim/treesitter.lua
@@ -1,9 +1,9 @@
 vim.api.nvim_create_autocmd('FileType', {
   callback = function (a)
     vim.schedule(function ()
-      if pcall(vim.treesitter.start, a.buf) then
+      if pcall(vim.treesitter.start, a.buf, a.match) then
         if vim.bo.indentexpr == '' then
-          vim.bo.indentexpr = vim.bo.indentexpr or 'v:lua.require("nvim-treesitter").indentexpr()'
+          vim.bo.indentexpr = 'v:lua.require("nvim-treesitter").indentexpr()'
         end
       end
     end)
